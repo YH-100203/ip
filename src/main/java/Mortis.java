ae744eb2aff0564b0443f93646755df33c1d4a32
YH-100203 <griezmannlyh1002@gmail.com>
2025-08-21 16:42:07 +0800

    Add input loop to read user commands

--- src/main/java/Mortis.java
+++ src/main/java/Mortis.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Mortis {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Mortis");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+
+        // Echo loop
+        while (true) {
+            // Standard code for input
+            String input = sc.nextLine();
+            System.out.println("    ____________________________________________________________");
+            System.out.println("     " + input); // To echo input
+            System.out.println("    ____________________________________________________________");
+            break;
+
+        }
+
     }
 }
