4fb8e38687b15367a37a742326e54ecffe6c4c54
YH-100203 <griezmannlyh1002@gmail.com>
2025-08-27 21:11:11 +0800

    Add date handling for deadlines with new format

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,10 +1,17 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 public class Deadline extends Task {
     protected String by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        LocalDate d1 = LocalDate.parse(by);
+        this.by = d1.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
     }
+
+
 
     @Override
     public String toString() {
